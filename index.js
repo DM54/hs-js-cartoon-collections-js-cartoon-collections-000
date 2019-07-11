7d67839bb6517bddf5ec67dcb158160be41d58e9
Dyana Monroy <dyana.monroy17@live.com>
2019-07-11 02:19:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 function dwarfRollCall(dwarves) {
+ // var arr = []
+ var a = ""
+  for(var i =0; i< dwarves.length; i++){
+  // arr.push( i+1 + ". " + dwarves[i] + "")
+    a += i+1 + '. ' +  dwarves[i] + " " 
+  }
+    return a
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var arr = []
+  for(var i =0; i <planeteerCalls.length; i++){
+    planeteerCalls[i] = planeteerCalls[i].toUpperCase()
+    arr.push(planeteerCalls[i] + "!")
+  }
+  return arr
 }
 
 function longPlaneteerCalls(words) {
+  var arr = []
+  for(var i =0; i<words.length; i++){
+
+  if(words[i].length >4){
+    return true
+  }
+  
+  else {
+    return false
+  }
+}
 }
 
 function findTheCheese (foods) {
+  var arr = []
+  for(var i =0; i<foods.length; i++){
+   if(foods.includes( "cheddar")){
+  
+       return "cheddar"
+   
+   
+  }
+   else  {
+   return "no cheese!"
+   }
+ }
 }
