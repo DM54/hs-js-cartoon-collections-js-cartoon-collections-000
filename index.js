8aab32973b6fd185791d6f9b20efab7621a53d71
Dyana Monroy <dyana.monroy17@live.com>
2019-07-11 18:13:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,7 +34,9 @@ function longPlaneteerCalls(words) {
 function findTheCheese (foods) {
   var arr = ["cheddar", "gouda", "camembert"]
   for(var i =0; i<foods.length; i++){
-    
+     if(arr.includes(foods[i])){
+       
+     }
     //"tomato soup", "cheddar", "oyster crackers", "gouda"
   /* if(foods.includes( "cheddar")){
   
